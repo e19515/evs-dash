3c59a71f8b351db95d769a2fa5384ed41bb4358f
e19515 <80969447+e19515@users.noreply.github.com>
2021-03-21 02:05:51 +0000

    Added: AmplifyAuthenticator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import { AmplifyAuthenticator, AmplifySignUp, AmplifySignIn, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AmplifyAuthenticator>
+      { /* Present the actual dashboard */ }
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Learn React
+          </a>
+          <AmplifySignOut />
+        </header>
+      </div>
+
+      { /* AmplifySignIn simplified for demo use */ }
+      <AmplifySignIn
+        slot="sign-in"
+        headerText="Sign in to ChargePoint dashboard"
+        formFields={[
+          {
+            type: "username",
+            label: "Username",
+            placeholder: "evsdemo",
+            required: true,
+          },
+          {
+            type: "password",
+            label: "Password",
+            placeholder: "evsdemo1",
+            required: true,
+          },
+        ]}
+        hideSignUp="true"
+      />
+      
+      { /* AmplifySignUp house-keeping, unimportant */ }
+      <AmplifySignUp
+        slot="sign-up"
+        formFields={[
+          {
+            type: "username",
+            label: "Username",
+            placeholder: "evsdemo",
+            required: true,
+          },
+          {
+            type: "password",
+            label: "Password",
+            placeholder: "evsdemo1",
+            required: true,
+          },
+        ]} 
+      />
+    </AmplifyAuthenticator>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Set up aws-amplify
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
