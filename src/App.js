1f6bb5a871b6cd30cb191c39cbfbae29b976c0c0
e19515 <80969447+e19515@users.noreply.github.com>
2021-03-25 13:08:52 +0000

    Rewrite App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,82 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import { AmplifyAuthenticator, AmplifySignUp, AmplifySignIn, AmplifySignOut } from '@aws-amplify/ui-react';
+import {AuthState} from "@aws-amplify/ui-components";
+import Helmet from "react-helmet";
+import { API, Auth } from 'aws-amplify';
+import { useState, useEffect } from 'react';
+import useInterval from 'use-interval';
+
+import ChargePointTable from './ChargePointTable';
 
 function App() {
+  const [data, setData] = useState([{FriendlyName: 'loading...'}])
+  
+  const fetchDashboardAPI = async function() {
+    const apiName = 'ChargePoint';
+    const path = '/dashboard';
+
+    Auth.currentAuthenticatedUser().then( cognitoUser =>{
+      API.get(apiName, path).then( result => {
+        console.debug(result.chargePoints);
+        setData(result.data);
+      }, apiReason => {
+        console.error(apiReason);
+      })
+    }, authReason => {
+      console.log(authReason);
+    })
+  }
+  
+  // fetchDashboardAPI on signedIn
+  const handleAuthStateChange = (nextAuthState) => {
+    if (nextAuthState === AuthState.SignedIn) {
+      fetchDashboardAPI();
+    }
+  };
+  // fetchDashboardAPI using the effect Hook, similar to componentDidMount (already signedIn)
+  useEffect( () => { 
+    fetchDashboardAPI();
+  }, []);
+  // fetchDashboardAPI every 30s
+  useInterval( () => {
+    fetchDashboardAPI();
+  }, 30*1000/* ms */);
+
   return (
-    <AmplifyAuthenticator>
-      { /* Present the actual dashboard */ }
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            Learn React
-          </a>
-          <AmplifySignOut />
-        </header>
+    <AmplifyAuthenticator handleAuthStateChange={handleAuthStateChange}>
+      {/* Present the actual Dashboard, apply Bootstrap layout */}
+      <div className="App container-fluid">
+        {/* FIXME: AmplifyAuthenticator conflict */}
+        <Helmet>
+          <title>ChargePoint - EVS dashboard</title> 
+        </Helmet>
+        <div className="row my-3">
+          <div className="col-md-9 col-sm-8">
+            <h1>
+              ChargePoint&nbsp;
+              <br className="d-md-none" />
+              <small className="text-muted">EVS dashboard</small>
+            </h1>
+          </div>
+          <div className="col-md-3 col-sm-4">
+            <AmplifySignOut />
+          </div>
+        </div>
+        <div className="row">
+
+
+          <ChargePointTable
+            data={data}
+          />
+
+        </div>
       </div>
 
-      { /* AmplifySignIn simplified for demo use */ }
+      {/* AmplifySignIn watered down for demo use */}
       <AmplifySignIn
         slot="sign-in"
-        headerText="Sign in to ChargePoint dashboard"
+        headerText="Sign in to EVS dashboard"
         formFields={[
           {
             type: "username",
@@ -43,26 +92,7 @@ function App() {
           },
         ]}
         hideSignUp="true"
-      />
-      
-      { /* AmplifySignUp house-keeping, unimportant */ }
-      <AmplifySignUp
-        slot="sign-up"
-        formFields={[
-          {
-            type: "username",
-            label: "Username",
-            placeholder: "evsdemo",
-            required: true,
-          },
-          {
-            type: "password",
-            label: "Password",
-            placeholder: "evsdemo1",
-            required: true,
-          },
-        ]} 
-      />
+      /> {/* NOTE: SignUp backend requires manual confirmation via aws congnito console so it's hidden */}
     </AmplifyAuthenticator>
   );
 }
